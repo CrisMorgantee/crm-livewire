7705b9d4eaa744175ed799389cc1c7445f86d4ed
Cristiano Morgante <cristianomorgante@hotmail.com>
2023-11-01 16:19:07 -0300

    crm-4: check form rules

--- app/Livewire/Auth/Password/Reset.php
+++ app/Livewire/Auth/Password/Reset.php
@@ -2,18 +2,33 @@
 
 namespace App\Livewire\Auth\Password;
 
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Reset extends Component
 {
     public ?string $token = null;
 
-    public function mount(): void
+    #[Rule(['required', 'email'])]
+    public ?string $email = null;
+
+    #[Rule(['required', 'confirmed'])]
+    public ?string $password = null;
+
+    #[Rule(['required'])]
+    public ?string $password_confirmation = null;
+
+    public function mount(?string $token = null, ?string $email = null): void
     {
-        $this->token = request('token');
+        $this->token = request('token', $token);
+        $this->email = request('email', $email);
 
         if ($this->tokenNotValid()) {
             session()->flash('status token', 'Invalid token.');
@@ -37,5 +52,25 @@ class Reset extends Component
         }
 
         return true;
+    }
+
+    public function updatePassword(): void
+    {
+        $this->validate();
+
+        $status = Password::reset(
+            $this->only(['email', 'password', 'password_confirmation', 'token']),
+            function(User $user, $password) {
+                $user->password       = $password;
+                $user->remember_token = Str::random(60);
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        session()->flash('status', __($status));
+
+        $this->redirect(route('dashboard'));
     }
 }
