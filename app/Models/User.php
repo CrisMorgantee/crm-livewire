5911186f6ac880b431e32fbeab7e1bd015d9583b
Cristiano Morgante <cristianomorgante@hotmail.com>
2023-11-22 17:26:32 -0300

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -29,4 +30,21 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password'          => 'hashed',
     ];
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function givePermissionTo(string $name): void
+    {
+        $this->permissions()->save(
+            Permission::firstOrNew(['name' => $name])
+        );
+    }
+
+    public function hasPermissionTo(string $name): bool
+    {
+        return $this->permissions->contains('name', $name);
+    }
 }
---
+++ tests/Feature/PermissionsControlTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Permission;
+use App\Models\User;
+
+use function Pest\Laravel\assertDatabaseHas;
+
+it('should be able to give an user a permission to do something', function() {
+    $user = User::factory()->create();
+
+    $user->givePermissionTo('doSomething');
+
+    expect($user)
+        ->hasPermissionTo('doSomething')
+        ->toBeTrue();
+
+    assertDatabaseHas('permissions', [
+        'name' => 'doSomething',
+    ]);
+
+    assertDatabaseHas('permission_user', [
+        'user_id'       => $user->id,
+        'permission_id' => Permission::whereName('doSomething')->first()->id,
+    ]);
+});
