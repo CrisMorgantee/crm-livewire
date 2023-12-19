539e89ce5d6c8fb6c2aa2194d638debb971717a6
Cristiano Morgante <cristianomorgante@hotmail.com>
2023-12-19 02:18:30 -0300

    crm-6: add users list table

--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -1,5 +1,15 @@
 <div>
-    @foreach($this->users as $user)
-        <span>{{$user->name}}</span>
-    @endforeach
+    <x-header title="Users" separator/>
+
+    <x-table :headers="$this->headers" :rows="$this->users">
+        @scope('cell_permissions', $user)
+        @foreach($user->permissions as $permission)
+            <x-badge :value="$permission->name" class="badge-primary"/>
+        @endforeach
+        @endscope
+
+        @scope('actions', $user)
+        <x-button icon="o-trash" class="btn-xs" wire:click="delete({{ $user->id }})" spinner/>
+        @endscope
+    </x-table>
 </div>
