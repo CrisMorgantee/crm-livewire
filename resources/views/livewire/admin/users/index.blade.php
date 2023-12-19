9b245754626d80022b16034a62fb68d7dbe46646
Cristiano Morgante <cristianomorgante@hotmail.com>
2023-12-19 01:27:12 -0300

    crm-6: let's create a livewire component to list all users in the page

---
+++ resources/views/livewire/admin/users/index.blade.php
@@ -0,0 +1,5 @@
+<div>
+    @foreach($this->users as $user)
+        <span>{{$user->name}}</span>
+    @endforeach
+</div>
--- tests/Feature/Admin/UsersManagment/UsersManagementTest.php
+++ tests/Feature/Admin/UsersManagment/UsersManagementTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Admin\Users\Index;
 use App\Models\User;
 
 use function Pest\Laravel\actingAs;
@@ -18,4 +19,17 @@ test('make sure that the route is protected by the permission BE_AN_ADMIN', func
     actingAs($user)
         ->get(route('admin.users'))
         ->assertForbidden();
+});
+
+test("let's create a livewire component to list all users in the page", function() {
+    $users = User::factory()->count(10)->create();
+
+    $lw = Livewire::test(Index::class)
+        ->assertSet('users', function($usr) use ($users) {
+            return $usr->count() === $users->count();
+        });
+
+    foreach ($users as $user) {
+        $lw->assertSee($user->name);
+    }
 });
